989c7729a64f09af648fb85389e739b9b828e094
MonkeyArya <53228785+MonkeyArya@users.noreply.github.com>
2024-07-01 16:29:05 -0400

    created base interactable

--- scripts/interactable.gd
+++ scripts/interactable.gd
@@ -10,3 +10,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if Input.is_action_pressed("ui_right"):
+		$Sprite2D.set_texture(second_texture) # Replace with function body.
+
+func _on_Sprite_texture_changed():
+	if $Sprite2D.texture != get_parent().first_texture:
+		await get_tree().create_timer(3).timeout
+		$Sprite2D.set_texture(first_texture)

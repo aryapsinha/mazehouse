99753119974af460225841033c88c54b2385a8fe
MonkeyArya <53228785+MonkeyArya@users.noreply.github.com>
2024-07-07 14:27:35 -0400

    made interactable a tool

--- scripts/interactable.gd
+++ scripts/interactable.gd
@@ -1,6 +1,10 @@
+@tool
 extends Area2D
-var first_texture = load("TEXTURE PATH HERE"); 
-var second_texture = load("TEXTURE PATH HERE");
+
+@export var first_texture = load("ex"); 
+@export var second_texture = load("TEXTURE PATH HERE");
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,12 +13,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+	$Sprite2D.set_texture(first_texture)
+	
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("press"):
 		$Sprite2D.set_texture(second_texture) # Replace with function body.
 
 func _on_Sprite_texture_changed():

a8e03281e3a557ba28a954644a13ad6b7ee772e3
MonkeyArya <53228785+MonkeyArya@users.noreply.github.com>
2024-07-24 23:45:13 -0400

    right half teleport

--- scripts/navigation.gd
+++ scripts/navigation.gd
@@ -13,16 +13,18 @@ const startroom = preload("res://scenes/start_room.tscn")
 const storageroom = preload("res://scenes/storageroom.tscn")
 const studyroom = preload("res://scenes/studyroom.tscn")
 
+const angsty = preload("res://scenes/angsty_room.tscn")
+const bathroom1 = preload("res://scenes/bathroom_1.tscn")
+const gamesroom = preload("res://scenes/gamesroom.tscn") 
+const library = preload("res://scenes/libraryroom.tscn")
+const hallway1 = preload("res://scenes/hallway_1.tscn")
+const hallway2 = preload("res://scenes/hallway_2.tscn")
+const hallway3 = preload("res://scenes/hallway_3.tscn")
+const hallway4 = preload("res://scenes/hallway_4.tscn")
 '''
-const gamesroom = preload('ex')
-const bathroom1 = preload('ex')
-const angsty 
-const library
 
-const hallway1 = preload('ex')
-const hallway2 = preload('ex')
-const hallway3 = preload('ex')
-const hallway4 = preload('ex')
+
+
 '''
 var spawndoortag
 # Called when the node enters the scene tree for the first time.
@@ -54,6 +56,22 @@ func go(leveltag):
 			sceneload = storageroom 
 		"studyroom":
 			sceneload = studyroom 
+		"library":
+			sceneload = library
+		"gamesroom":
+			sceneload = gamesroom
+		"angsty":
+			sceneload = angsty
+		"bathroom1":
+			sceneload = bathroom1
+		"hallway1":
+			sceneload = hallway1
+		"hallway2":
+			sceneload = hallway2
+		"hallway3":
+			sceneload = hallway3
+		"hallway4":
+			sceneload = hallway4
 	if sceneload != null:
 		#spawndoortag = spawndoor
 		
